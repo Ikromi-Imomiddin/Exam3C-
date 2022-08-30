c01ce49296d5295f04b28329d5c8ee6509002ee8
Ikromi Imomiddin <110010612+Imomiddinkiller@users.noreply.github.com>
2022-08-30 19:28:27 +0500

    I have fixed errors

---
+++ Solution/Domain/Models/Employee.cs
@@ -0,0 +1,24 @@
+namespace Domain.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public Department departments { get; set; }
+    public List<Role> Roles { get; set; }
+    public List<Experience> Experiences   { get; set; }
+
+    public void GetInfo()
+    {
+        System.Console.WriteLine($"{FirstName} {LastName}");
+        System.Console.WriteLine($"Departmetnt: {departments}");
+        System.Console.WriteLine($"Experiences: {Experiences}");
+        System.Console.WriteLine($"Roles: {Roles}");
+        System.Console.WriteLine("    ");
+    }
+}
+
